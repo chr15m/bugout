5a50abd30e403d6d2b30fb5ad06bde8f35b8462e
Chris McCormick <chris@mccormick.cx>
2018-08-26 15:19:41 +0800

    More instantiation tests.

--- test.js
+++ test.js
@@ -8,19 +8,25 @@ test.onFinish(function() {
   wtest.destroy();
 });
 
-test('Testing instantiation', function (t) {
-  t.plan(4);
+test('Instantiation', function (t) {
+  t.plan(6);
 
-  var b = new Bugout({seed: "BohNtZ24TrgMwZTLx9VDKtcZARNVuCt5tnecAAxYtTBC8pC61uGN", wt: wtest});
-  t.equal(b.identifier, "bYSkTy24xXJj6dWe79ZAQXKJZrn2n983SQ", "identifier");
-  t.equal(b.pk, "CXENBY9X3x5TN1yjRyu1U1WkGuujuVBNiqxA16oAYbFo", "public key");
-  b.torrent.on("infoHash", function() {
-    t.equal(b.torrent.infoHash, "28d878040b7d2f5215409373b415fb99bc0e6d88", "infoHash");
+  var b1 = new Bugout({seed: "BohNtZ24TrgMwZTLx9VDKtcZARNVuCt5tnecAAxYtTBC8pC61uGN", wt: wtest});
+  t.equal(b1.identifier, "bYSkTy24xXJj6dWe79ZAQXKJZrn2n983SQ", "server identifier");
+  t.equal(b1.pk, "CXENBY9X3x5TN1yjRyu1U1WkGuujuVBNiqxA16oAYbFo", "server public key");
+  b1.torrent.on("infoHash", function() {
+    t.equal(b1.torrent.infoHash, "28d878040b7d2f5215409373b415fb99bc0e6d88", "server infoHash");
   });
 
   try {
-    var b = new Bugout({seed: "BohNtZ24TrgMwZTLx9VDLtcZARNVuCt5tnecAAxYtTBC8pC61uGN", wt: wtest});
+    var b2 = new Bugout({seed: "BohNtZ24TrgMwZTLx9VDLtcZARNVuCt5tnecAAxYtTBC8pC61uGN", wt: wtest});
   } catch(e) {
     t.equal(e.toString(), "Error: Invalid checksum", "invalid seed checksum");
   }
-})
+  
+  var b3 = new Bugout("bMuHpwCxcD5vhC5u7VKuajYu5RU7FUnaGJ", {wt: wtest});
+  t.equal(b3.identifier, "bMuHpwCxcD5vhC5u7VKuajYu5RU7FUnaGJ", "client identifier");
+  b3.torrent.on("infoHash", function() {
+    t.equal(b3.torrent.infoHash, "d96fe55834a62d86e48573c132345c01a38f5ffd", "client infoHash");
+  });
+});
